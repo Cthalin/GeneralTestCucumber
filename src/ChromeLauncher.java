8bbb2f087ddc589c85a28ed18c43f57f8a1f47b4
eslowikowski <erik.slowikowski@channelpilot.com>
2018-01-16 15:16:37 +0100

    Feed Import outsourced

--- src/ChromeLauncher.java
+++ src/ChromeLauncher.java
@@ -18,7 +18,7 @@ public class ChromeLauncher {
 
     public static void main(String[] args) throws InterruptedException {
         //Start Chrome Browser driver
-        ChromeOptions capabilities = new ChromeOptions();;
+        ChromeOptions capabilities = new ChromeOptions();
         driver = new ChromeDriver(capabilities);
         wait = new WebDriverWait(driver, 10);
         waitLong = new WebDriverWait(driver, 600);
@@ -39,36 +39,8 @@ public class ChromeLauncher {
         testShopCreation.createShop(wait,driver,testData);
 
         //Import Feed
-        Thread.sleep(5000);
-        WebElement title = driver.findElement(By.cssSelector("h1.shop-selected-title"));
-        waitLong.until(ExpectedConditions.attributeToBe(title,"innerText",testData.shopTitle));
-        WebElement myDynElement = (new WebDriverWait(driver,5)).until(ExpectedConditions.elementToBeClickable(By.cssSelector("ul.category-navigation a[href=\"#catalog\"]")));
-        myDynElement.click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-source label.type")));
-        Select typeSelect = new Select(driver.findElement(By.cssSelector("form.catalog-source select.type")));
-        Thread.sleep(1000);
-        typeSelect.selectByValue("http");
-        Thread.sleep(1000);
-        je.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(By.cssSelector("div.catalog-source div.type.http input.url")));
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.catalog-source div.type.http input.url"))).sendKeys(testData.feedUrl);
-        Thread.sleep(1000);
-
-        driver.findElement(By.cssSelector("div.single-feed-config form.catalog-source div.submit input")).click();
-        waitLong.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-source div.response-messages")));
-        assertTrue("Feedimport fehlgeschlagen",importSuccessShown());
-        assertFalse("Feedimport erfolgreich", importSuccessShown());
-
-        //Format
-        je.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(By.cssSelector("div.catalog-format div.submit")));
-        driver.findElement(By.cssSelector("div.catalog-format div.submit")).click();
-        waitLong.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-format div.response-messages")));
-        assertTrue("Formatprüfung fehlgeschlagen",formatSuccessShown());
-        assertFalse("Formatprüfung erfolgreich", formatSuccessShown());
-
-        //Save
-        je.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(By.cssSelector("div.save-settings div.submit")));
-        driver.findElement(By.cssSelector("div.save-settings div.submit")).click();
-        waitLong.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.row.feed-overview")));
+        TestFeedImport testFeedImport = new TestFeedImport();
+        testFeedImport.importFeed(wait,driver,testData,waitLong,je);
 
         //Export Feed
         driver.findElement(By.cssSelector("div.menu-bar a.button.price-comparison-sites")).click();
@@ -90,22 +62,14 @@ public class ChromeLauncher {
         driver.quit();
     }
 
-    public static boolean importSuccessShown(){
-        waitLong.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-source div.response-messages")));
-        return driver.findElement(By.cssSelector("div.catalog-source div.response-messages")).findElements(By.cssSelector("p.type-success")).size() == 1;
-    }
 
-    public static boolean formatSuccessShown(){
-        waitLong.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-source div.response-messages")));
-        return driver.findElement(By.cssSelector("div.catalog-format div.response-messages")).findElements(By.cssSelector("p.type-success")).size() == 1;
-    }
 
-    public static boolean testChannelSetup(){
+    private static boolean testChannelSetup(){
         waitLong.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body > div.page > div > div.app.pcs-manager > div > div > div.twelve.columns.channel-overview > ul > li:nth-child(2) > a")));
         return driver.findElement(By.cssSelector("body > div.page > div > div.app.pcs-manager > div > div > div.twelve.columns.channel-overview > ul > li:nth-child(2) > a")).findElements(By.cssSelector("a[title=\"googleShopping (DE)\"]")).size() == 1;
     }
 
-    public static void login(TestData testData){
+    private static void login(TestData testData){
         driver.findElement(By.name("email")).click();
         driver.findElement(By.name("email")).sendKeys(testData.user);
         WebElement parent = driver.findElement(By.className("password"));
@@ -115,7 +79,7 @@ public class ChromeLauncher {
         driver.findElement(By.cssSelector("nav.start a[href=\"/start/shops\"]")).click();
     }
 
-    public static void logout(){
+    private static void logout(){
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.category-context")));
         driver.findElement(By.cssSelector("div.navigation-service a.button")).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.navigation-service a[href=\"/service/logout\"]"))).click();
--- src/TestFeedImport.java
+++ src/TestFeedImport.java
@@ -1,2 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class TestFeedImport {
+
+    public void importFeed(WebDriverWait wait, WebDriver driver, TestData testData, WebDriverWait waitLong, JavascriptExecutor je) throws InterruptedException {
+        Thread.sleep(5000);
+        WebElement title = driver.findElement(By.cssSelector("h1.shop-selected-title"));
+        waitLong.until(ExpectedConditions.attributeToBe(title,"innerText",testData.shopTitle));
+        WebElement myDynElement = (new WebDriverWait(driver,5)).until(ExpectedConditions.elementToBeClickable(By.cssSelector("ul.category-navigation a[href=\"#catalog\"]")));
+        myDynElement.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-source label.type")));
+        Select typeSelect = new Select(driver.findElement(By.cssSelector("form.catalog-source select.type")));
+        Thread.sleep(1000);
+        typeSelect.selectByValue("http");
+        Thread.sleep(1000);
+        je.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(By.cssSelector("div.catalog-source div.type.http input.url")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.catalog-source div.type.http input.url"))).sendKeys(testData.feedUrl);
+        Thread.sleep(1000);
+
+        driver.findElement(By.cssSelector("div.single-feed-config form.catalog-source div.submit input")).click();
+        waitLong.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-source div.response-messages")));
+        assertTrue("Feedimport fehlgeschlagen",importSuccessShown(waitLong,driver));
+        assertFalse("Feedimport erfolgreich", importSuccessShown(waitLong,driver));
+
+        //Format
+        je.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(By.cssSelector("div.catalog-format div.submit")));
+        driver.findElement(By.cssSelector("div.catalog-format div.submit")).click();
+        waitLong.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-format div.response-messages")));
+        assertTrue("Formatprüfung fehlgeschlagen",formatSuccessShown(waitLong,driver));
+        assertFalse("Formatprüfung erfolgreich", formatSuccessShown(waitLong,driver));
+
+        //Save
+        je.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(By.cssSelector("div.save-settings div.submit")));
+        driver.findElement(By.cssSelector("div.save-settings div.submit")).click();
+        waitLong.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.row.feed-overview")));
+    }
+
+    private static boolean importSuccessShown(WebDriverWait waitLong, WebDriver driver){
+        waitLong.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-source div.response-messages")));
+        return driver.findElement(By.cssSelector("div.catalog-source div.response-messages")).findElements(By.cssSelector("p.type-success")).size() == 1;
+    }
+
+    private static boolean formatSuccessShown(WebDriverWait waitLong, WebDriver driver){
+        waitLong.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.catalog-source div.response-messages")));
+        return driver.findElement(By.cssSelector("div.catalog-format div.response-messages")).findElements(By.cssSelector("p.type-success")).size() == 1;
+    }
 }
